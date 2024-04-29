9f1f04527fc98562fc285c5f97f0ba542509a406
SzilagyiZsolt <zsoltika20030@gmail.com>
2024-04-29 09:55:29 +0200

    Add project files.

---
+++ Pizza/Database.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Pizza
+{
+    internal class Database
+    {
+        MySqlCommand sqlCommand;
+        MySqlConnection connection;
+
+        public Database()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "";
+            builder.Database = "pizza";
+            connection = new MySqlConnection(builder.ConnectionString);
+            sqlCommand = connection.CreateCommand();
+            try
+            {
+                kapcsolatnyit();
+                kapcsolatzar();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
+        private void kapcsolatzar()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        private void kapcsolatnyit()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+        internal List<Futar> getAllfutarok()
+        {
+            List<Futar> futarok = new List<Futar>();
+            sqlCommand.CommandText = "SELECT futar.fnev, SUM(db*pizza.par) as ertek from futar NATURAL JOIN rendeles NATURAL JOIN tetel NATURAL JOIN pizza GROUP BY(futar.fnev) ORDER BY `ertek` ASC";
+            kapcsolatnyit();
+            using (MySqlDataReader dr = sqlCommand.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    Futar fut = new Futar(dr.GetInt32("fazon"), dr.GetString("fnev"), dr.GetString("ftel"), dr.GetInt32("ertek"));
+                    futarok.Add(fut);
+                }
+            }
+            return futarok;
+        }
+    }
+}
---
+++ Pizza/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    internal class Program
+    {
+        static Database db = new Database();
+        static List<Futar> futarok = new List<Futar>();
+        static void Main(string[] args)
+        {
+            futarok = db.getAllfutarok();
+            feladat01();
+            feladat02();
+            //feladat03();
+            Console.WriteLine("Program vége");
+            Console.ReadLine();
+        }
+
+        //private static void feladat03()
+        //{
+        //    //Hányan dolgoznak az egyes részlegeken
+        //    Console.WriteLine("3. feladat");
+        //    foreach (var dolgozo in dolgozok.GroupBy(a => a.reszleg).Select(b => new { reszleg = b.Key, letszam = b.Count() }).OrderBy(x => x.letszam))
+        //    {
+        //        Console.WriteLine($"\t{dolgozo.reszleg} - {dolgozo.letszam} fő");
+        //    }
+        //}
+        //
+        private static void feladat02()
+        {
+            //A legkisebb értékben értékesítő futár nevét írja ki 
+            Console.WriteLine("2. feladat");
+            foreach (var f in futarok)
+            {
+                Console.WriteLine($"\t{f.fnev} - {f.ertek}");
+            }
+        }
+
+        private static void feladat01()
+        {
+            //Írja ki a futárok adatait
+            Console.WriteLine("1. feladat");
+            foreach (var f in futarok)
+            {
+                Console.WriteLine($"\t{f.fazon}, {f.fnev}, {f.ftel}");
+            }
+        }
+    }
+}
